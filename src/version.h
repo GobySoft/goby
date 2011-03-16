fa75372746f84da4d5cc98796cf710a36d6036b6
toby <tes.aubergine@gmail.com>
2011-03-16 15:03:46 -0400

    incremented version #

--- src/version.h
+++ src/version.h
@@ -23,8 +23,8 @@
 
 namespace goby
 {
-    const std::string VERSION_STRING = "1.0.1";
-    const std::string VERSION_DATE = "2011 Mar 03";    
+    const std::string VERSION_STRING = "1.0.2";
+    const std::string VERSION_DATE = "2011 Mar 16";  
 }
 
 #endif

cf6e07297a2da35ace98e5b9a6aa7426dd51ec25
toby <toby@aubergine>
2011-04-06 16:47:08 -0400

    turned off unneeded MOOS test from building

--- scripts/test_suite.sh
+++ scripts/test_suite.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-pushd ../build
-ctest -D ExperimentalStart
-ctest -D ExperimentalUpdate
-ctest -D ExperimentalConfigure
-ctest -D ExperimentalBuild
-ctest -D ExperimentalTest
-ctest -D ExperimentalMemCheck
-ctest -D ExperimentalCoverage
-ctest -D ExperimentalSubmit
+pushd ~/goby/1.0/build
+ctest -D NightlyStart
+ctest -D NightlyUpdate
+ctest -D NightlyConfigure
+ctest -D NightlyBuild
+#ctest -D NightlyTest
+#ctest -D NightlyMemCheck
+#ctest -D NightlyCoverage
+ctest -D NightlySubmit
+popd

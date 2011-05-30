5b9a0abd722e4d05162203680294fcf828fcaf53
toby <tes.aubergine@gmail.com>
2011-05-30 16:06:55 -0400

    added test for multi-frame ack

--- src/doc/user_manual/build_user_manual.sh
+++ src/doc/user_manual/build_user_manual.sh
@@ -12,16 +12,16 @@ CMAKE_CURRENT_BINARY_DIR=$3
 OUTPUT=$4
 
 if [[ "$1" == "quick" ]]; then
-    xelatex -halt-on-error ${CMAKE_CURRENT_BINARY_DIR}/user_manual.tex
+    xelatex -shell-escape -halt-on-error ${CMAKE_CURRENT_BINARY_DIR}/user_manual.tex
     cp user_manual.pdf $OUTPUT
 else    
     rm -f ${CMAKE_CURRENT_BINARY_DIR}/user_manual.aux
     pushd ${CMAKE_CURRENT_BINARY_DIR}
-    xelatex user_manual.tex -halt-on-error -no-pdf
+    xelatex -shell-escape user_manual.tex -halt-on-error -no-pdf
     bibtex user_manual
     makeglossaries user_manual
-    xelatex user_manual.tex -halt-on-error -no-pdf
-    xelatex user_manual.tex
+    xelatex -shell-escape user_manual.tex -halt-on-error -no-pdf
+    xelatex -shell-escape user_manual.tex
     cp user_manual.pdf $OUTPUT
     popd
 fi

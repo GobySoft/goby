ef1c2c606cafd1761dc7e9e8baf36c0a8040be09
toby <tes.aubergine@gmail.com>
2011-06-06 16:25:02 -0400

    version 1.1.0

--- src/version.h
+++ src/version.h
@@ -19,12 +19,14 @@
 #ifndef VERSION20110304H
 #define VERSION20110304H
 
-#include <string>
+#define GOBY_VERSION_MAJOR @GOBY_VERSION_MAJOR@
+#define GOBY_VERSION_MINOR @GOBY_VERSION_MINOR@
+#define GOBY_VERSION_PATCH @GOBY_VERSION_PATCH@
 
 namespace goby
 {
-    const std::string VERSION_STRING = "1.0.2b";
-    const std::string VERSION_DATE = "2011 Mar 29";  
+    const std::string VERSION_STRING = "@GOBY_VERSION@";
+    const std::string VERSION_DATE = "@GOBY_VERSION_DATE@";
 }
 
 #endif

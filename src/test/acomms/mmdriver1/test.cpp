e91b8d81f3a7a92bf3b0d61ea22a21ef3ee56ef4
Toby Schneider <tes.aubergine@gmail.com>
2011-08-24 21:13:12 -0400

    added 1 second wait between tests for MicroModem to recover

--- src/test/acomms/mmdriver1/test.cpp
+++ src/test/acomms/mmdriver1/test.cpp
@@ -133,6 +133,7 @@ int main(int argc, char* argv[])
             goby::glog << "Test " << group("test") << test_number << " passed." << std::endl;
             ++test_number;
             check_count = 0;
+            sleep(1);
             
         }
     }

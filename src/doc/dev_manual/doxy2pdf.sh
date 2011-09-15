3c863b6b529186a99637f3fe817f2edd3a9c826e
Toby Schneider <tes.aubergine@gmail.com>
2011-09-14 21:40:35 -0400

    updated documentation - mostly preliminarily done except rest of WHOI Micro-Modem NMEA mapping

--- src/doc/dev_manual/doxy2pdf.sh
+++ src/doc/dev_manual/doxy2pdf.sh
@@ -5,7 +5,9 @@ CMAKE_CURRENT_SOURCE_DIR=$1
 CMAKE_CURRENT_BINARY_DIR=$2
 OUTPUT=$3
 
-((cat ${CMAKE_CURRENT_BINARY_DIR}/goby-dev.doxy; bzr version-info --custom --template="PROJECT_NUMBER = \"Series: {branch_nick}, revision: {revno}, released on {date} \"") | doxygen - 
+# ((cat ${CMAKE_CURRENT_BINARY_DIR}/goby-dev.doxy; bzr version-info --custom --template="PROJECT_NUMBER = \"Series: {branch_nick}, revision: {revno}, released on {date} \"") | doxygen - 
+
+(doxygen ${CMAKE_CURRENT_BINARY_DIR}/goby-dev.doxy
 
 pushd $CMAKE_CURRENT_BINARY_DIR/latex
 cat refman.tex | sed 's|\\chapter{Namespace Index}|\\appendix\\chapter{Namespace Index}|'  | sed 's|\\include|\\input|' > refman2.tex

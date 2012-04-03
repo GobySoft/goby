007f07fc650c61f7d65e813f181a71b61c84a8fe
Toby Schneider <tes.aubergine@gmail.com>
2012-04-03 09:54:01 -0400

    put new header on goby-3 files that needed it

--- src/pb/sandbox/pgm_test/pgmtest1.cpp
+++ src/pb/sandbox/pgm_test/pgmtest1.cpp
@@ -1,3 +1,25 @@
+// Copyright 2009-2012 Toby Schneider (https://launchpad.net/~tes)
+//                     Massachusetts Institute of Technology (2007-)
+//                     Woods Hole Oceanographic Institution (2007-)
+//                     Goby Developers Team (https://launchpad.net/~goby-dev)
+// 
+//
+// This file is part of the Goby Underwater Autonomy Project Libraries
+// ("The Goby Libraries").
+//
+// The Goby Libraries are free software: you can redistribute them and/or modify
+// them under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Goby Libraries are distributed in the hope that they will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Goby.  If not, see <http://www.gnu.org/licenses/>.
+
 //
  //  Weather update server in C++
 //  Binds PUB socket to tcp://*:5556

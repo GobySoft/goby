82ecbd55764f67c72581c39de5dd68fce2df69c6
Toby Schneider <tes.aubergine@gmail.com>
2013-10-16 13:17:22 -0400

    Modified headers on seawater calculations

--- src/util/seawater/pressure.h
+++ src/util/seawater/pressure.h
@@ -1,22 +1,29 @@
-// s. petillo spetillo@mit.edu 07.22.10
-// ocean engineering graduate student - mit / whoi joint program
-// massachusetts institute of technology (mit)
-// laboratory for autonomous marine sensing systems (lamss)
+// Copyright 2009-2013 Toby Schneider (https://launchpad.net/~tes)
+//                     Massachusetts Institute of Technology (2007-)
+//                     Woods Hole Oceanographic Institution (2007-)
+//                     Goby Developers Team (https://launchpad.net/~goby-dev)
 // 
-// this is pressure.h
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
+// This file is part of the Goby Underwater Autonomy Project Libraries
+// ("The Goby Libraries").
+//
+// The Goby Libraries are free software: you can redistribute them and/or modify
+// them under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// This software is distributed in the hope that it will be useful,
+// The Goby Libraries are distributed in the hope that they will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this software.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public License
+// along with Goby.  If not, see <http://www.gnu.org/licenses/>.
+
+// modified for C++ by s. petillo spetillo@mit.edu 
+// ocean engineering graduate student - mit / whoi joint program
+// massachusetts institute of technology (mit)
+// laboratory for autonomous marine sensing systems (lamss)
 
 #ifndef PRESSUREH
 #define PRESSUREH

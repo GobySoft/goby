2660e58aaffcf73158a5cd04393575d86c83d07f
Toby Schneider <toby@gobysoft.org>
2017-07-14 16:04:29 -0400

    Removed fixed buffer size now that MOOS10 has correct size method

--- src/moos/moos_serializer.h
+++ src/moos/moos_serializer.h
@@ -37,8 +37,7 @@ namespace goby
                 // copy because Serialize wants to modify the CMOOSMsg
                 CMOOSMsg msg(const_msg);
                 // adapted from MOOSCommPkt.cpp
-                const unsigned PKT_TMP_BUFFER_SIZE = 40000;
-                int serialized_size = PKT_TMP_BUFFER_SIZE;
+                int serialized_size = msg.GetSizeInBytesWhenSerialised();
                 data->resize(serialized_size);
                 serialized_size = msg.Serialize(reinterpret_cast<unsigned char*>(&(*data)[0]), serialized_size);
                 data->resize(serialized_size);
